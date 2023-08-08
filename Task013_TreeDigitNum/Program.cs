486b84ec403e9dd405098665b3d676f21930829c
Андрей Репринцев <Randrew8313@gmail.com>
2023-08-08 23:49:12 +0300

    Задача 13

---
+++ Task013_TreeDigitNum/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int num = new Random().Next(0, 10000);
+Console.Write("Заданное число: ");
+Console.WriteLine(num);
+
+if(num > 99)
+{
+    while(num > 1000)
+    {
+        num = num / 10;
+    }
+    num = num % 10;
+    Console.Write("Третья цифра заданного числа: ");
+    Console.WriteLine(num);
+}
+else
+{
+    Console.Write("Третьей цифры нет.");
+}
