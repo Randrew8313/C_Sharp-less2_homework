2286abecd3a74ce1aab385a9f5690479700eaaf7
Андрей Репринцев <Randrew8313@gmail.com>
2023-08-08 23:25:08 +0300

    Задача 10

---
+++ Task010_SecDigitNumb/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int num = new Random().Next(0, 1000);
+Console.Write("Исходное число: ");
+Console.WriteLine(num);
+
+if((num / 100 > 0) && (num / 100 < 10))
+{
+    int SecondDigit = num % 100;
+    SecondDigit = SecondDigit / 10;
+    Console.Write("Вторая цифра этого числа: ");
+    Console.WriteLine(SecondDigit);
+}
+else
+{
+    Console.WriteLine("Число не трехзначное!");
+}
